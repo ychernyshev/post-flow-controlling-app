e46526afc1b44ca7ab610675e075ca85f75b46e7
Eugene <eugene_chernyshev@yahoo.com>
2025-12-01 11:07:52 +0200

    Postage item in model
    The model "postageItemModel" has been added to the models.py

--- flow_app_core/models.py
+++ flow_app_core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class postageItemModel(models.Model):
+    track_number = models.CharField(max_length=13, verbose_name='Track number')
+    recipient_street = models.CharField(max_length=20, verbose_name='Recipient street')
+    delivered_date = models.DateField(auto_now_add=False)
+
+    def __str__(self):
+        return f'{self.track_number}: {self.recipient_street}'
+
+    class Meta:
+        ordering = ['delivered_date']
+        verbose_name = 'postage item'
+        verbose_name_plural = 'Postage items'
