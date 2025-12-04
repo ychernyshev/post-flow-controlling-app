8d2ed1ed4754ba9530610fd90dc2f102daea27cc
Eugene <eugene_chernyshev@yahoo.com>
2025-12-04 10:51:26 +0200

    Update admin.py
    Added CRUD operations for postageItemModel in the admin.py

--- flow_app_core/admin.py
+++ flow_app_core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from flow_app_core.models import postageItemModel
+
+
 # Register your models here.
+@admin.register(postageItemModel)
+class postageItemAdmin(admin.ModelAdmin):
+    list_dislpay = ['track_number', 'recipient_street', 'delivered_date']
