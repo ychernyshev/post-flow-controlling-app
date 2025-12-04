bd49fbaaaa9a1bb031135975aed487b360bbb3ed
Eugene <eugene_chernyshev@yahoo.com>
2025-12-04 10:49:28 +0200

    API essue fixing
    Added "class Meta"

--- flow_app_core/serializers.py
+++ flow_app_core/serializers.py
@@ -4,6 +4,7 @@ from flow_app_core.models import postageItemModel
 
 
 class PostageItemSerializer(serializers.ModelSerializer):
-    model = postageItemModel
-    fields = '__all__'
+    class Meta:
+        model = postageItemModel
+        fields = '__all__'
 
