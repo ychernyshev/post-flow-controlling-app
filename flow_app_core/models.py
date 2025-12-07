70ed7a65c3f9975715465c1a52cc990986205fc5
Eugene <eugene_chernyshev@yahoo.com>
2025-12-07 11:07:07 +0200

    DateTimeField now instead of DateField
    DateField in the postageItemModel was replaced with the DateTimeField

--- flow_app_core/models.py
+++ flow_app_core/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class postageItemModel(models.Model):
     track_number = models.CharField(max_length=13, verbose_name='Track number')
     recipient_street = models.CharField(max_length=20, verbose_name='Recipient street')
-    delivered_date = models.DateField(auto_now_add=False)
+    delivered_date = models.DateTimeField(auto_now_add=False)
 
     def __str__(self):
         return f'{self.track_number}: {self.recipient_street}'
