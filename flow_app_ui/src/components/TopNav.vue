904865dbe437c5cb3d3f448e29dc43ab24bebbfd
Eugene <eugene_chernyshev@yahoo.com>
2025-12-08 20:41:10 +0200

    Add navbar
    Navbar has been added

--- flow_app_ui/src/components/TopNav.vue
+++ flow_app_ui/src/components/TopNav.vue
@@ -3,7 +3,28 @@
 </script>
 
 <template>
-
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Post-Flow Controlling App</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Історія ПВ</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Додати ПВ</a>
+          </li>
+        </ul>
+<!--        Search panel-->
+      </div>
+    </div>
+  </nav>
 </template>
 
 <style scoped>
