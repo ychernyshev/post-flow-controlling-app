28855e665c330cd96ddd71ad66df030a39ce60cb
Eugene <eugene_chernyshev@yahoo.com>
2025-12-08 20:37:10 +0200

    Add search panel
    Search panel has been added

--- flow_app_ui/src/components/SearchPanel.vue
+++ flow_app_ui/src/components/SearchPanel.vue
@@ -3,7 +3,10 @@
 </script>
 
 <template>
-
+  <form class="d-flex" role="search">
+    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success" type="submit">Search</button>
+  </form>
 </template>
 
 <style scoped>
