17bdc3515c68564c25479365f2270f3043a6c4de
Eugene <eugene_chernyshev@yahoo.com>
2025-12-11 21:15:42 +0200

    Default search input style
    Rolled back a search panel to the default view as a long top nav input

--- flow_app_ui/src/components/SearchPanel.vue
+++ flow_app_ui/src/components/SearchPanel.vue
@@ -3,14 +3,12 @@
 </script>
 
 <template>
-  <form class="d-flex" role="search">
-    <input class="form-control form-control-dark w-100 rounded-0 border-0" type="search" placeholder="Search" aria-label="Search">
-    <div class="navbar-nav">
-      <div class="nav-item text-nowrap">
-        <a class="nav-link px-3" href="#">Sign out</a>
-      </div>
+  <input class="form-control form-control-dark w-100 rounded-0 border-0" type="search" placeholder="Search" aria-label="Search">
+  <div class="navbar-nav">
+    <div class="nav-item text-nowrap">
+      <a class="nav-link px-3" href="#">Sign out</a>
     </div>
-  </form>
+  </div>
 </template>
 
 <style scoped>
