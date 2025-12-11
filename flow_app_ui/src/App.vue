7003f743e28baacc7114ec4b9b1538286f1e407a
Eugene <eugene_chernyshev@yahoo.com>
2025-12-11 20:07:48 +0200

    SideBar fixxes and setup
    - SideBar code has been fixed as in a Bootstrap free template
    - SideBar has been shown on the page as a visual component

--- flow_app_ui/src/App.vue
+++ flow_app_ui/src/App.vue
@@ -1,20 +1,23 @@
 <script setup>
   import TopNav from "./components/TopNav.vue";
+  import SideBar from "./components/SideBar.vue";
 </script>
 
 <template>
+  <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    <TopNav />
+  </header>
   <div class="container-fluid">
     <div class="row">
-      <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <TopNav />
-      </header>
+      <SideBar />
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
 
       </main>
-      <footer>
-
-      </footer>
     </div>
+
+    <footer>
+
+    </footer>
   </div>
 </template>
 
