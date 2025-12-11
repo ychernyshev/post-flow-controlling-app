d3d145865df4d92edb9e3fe8401df1576e1f23c4
Eugene <eugene_chernyshev@yahoo.com>
2025-12-11 20:26:29 +0200

    Update routes
    The route list has been brought into line with the current technical specifications of the project

--- flow_app_ui/src/router/index.js
+++ flow_app_ui/src/router/index.js
@@ -3,7 +3,11 @@ import HomeView from '../views/Dashboard.vue'
 
 const routes = [
     { path: '/', name: 'home', component: HomeView },
+    { path: '/dashboard', name: 'home', component: HomeView },
     { path: '/add_entry', name: 'about', component: () => import('../views/AddEntry.vue') },
+    { path: '/edit_entry', name: 'about', component: () => import('../views/EditEntry.vue') },
+    { path: '/entries_history', name: 'about', component: () => import('../views/EntriesHistory.vue') },
+    { path: '/search_results', name: 'about', component: () => import('../views/SearchResults.vue') },
 ]
 
 const router = createRouter({
