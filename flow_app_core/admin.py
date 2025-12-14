95b55c4e774a3cf5e760b7b1fa94189dcfa58b64
Eugene <eugene_chernyshev@yahoo.com>
2025-12-14 10:36:03 +0200

    Update admin.py
    Fixed syntax error in the "list_display" name

--- flow_app_core/admin.py
+++ flow_app_core/admin.py
@@ -6,4 +6,4 @@ from flow_app_core.models import PostageItemModel
 # Register your models here.
 @admin.register(PostageItemModel)
 class PostageItemAdmin(admin.ModelAdmin):
-    list_dislpay = ['track_number', 'recipient_street', 'delivered_date']
+    list_display = ['track_number', 'recipient_street', 'delivered_date']
