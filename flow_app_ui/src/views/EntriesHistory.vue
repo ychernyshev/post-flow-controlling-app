a3791e6b1cc6bb35f630123bc32653edef70c2fc
Eugene <eugene_chernyshev@yahoo.com>
2025-12-22 19:23:33 +0200

    Update EntriesHistory.vue
    Added basic get functionality

--- flow_app_ui/src/views/EntriesHistory.vue
+++ flow_app_ui/src/views/EntriesHistory.vue
@@ -1,11 +1,25 @@
-<script setup lang="ts">
+<script setup>
+  import {ref, onMounted} from "vue";
+  import {useAxios} from "../composables/useAxios.js";
 
+  const apiItems = useAxios('api/postage/');
+
+  const items = ref([])
+
+  onMounted(async () => {
+    items.value = await apiItems.get()
+  })
 </script>
 
 <template>
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">Entries History</h1>
   </div>
+  <ul>
+    <li v-for="item in items" :key="item.id">
+      {{ item }}
+    </li>
+  </ul>
 </template>
 
 <style scoped>
