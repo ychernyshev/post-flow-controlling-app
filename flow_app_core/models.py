63bf5cd2aaf34ea7e43369195b7a52f9a64bd72b
Eugene <eugene_chernyshev@yahoo.com>
2025-12-23 19:28:18 +0200

    Streets list moved
    - Removed the StreetsModel
    - Streets list moved to the PostageItemModel as a CharField

--- flow_app_core/models.py
+++ flow_app_core/models.py
@@ -5,20 +5,6 @@ from django.db import models
 
 # Create your models here.
 class PostageItemModel(models.Model):
-    track_number = models.CharField(max_length=13, verbose_name='Track number')
-    recipient_street = models.ForeignKey('StreetsModel', db_index=True, on_delete=models.PROTECT, verbose_name='Recipient street')
-    delivered_date = models.DateTimeField()
-
-    def __str__(self):
-        return f'{self.track_number}: {self.recipient_street}'
-
-    class Meta:
-        ordering = ['delivered_date']
-        verbose_name = 'postage item'
-        verbose_name_plural = 'Postage items'
-
-
-class StreetsModel(models.Model):
     STREETS = [
         ('', ''),
         ('SHKI', 'Шкільна'),
@@ -36,11 +22,14 @@ class StreetsModel(models.Model):
         ('BAND', 'С. Бандери'),
     ]
 
-    streets = models.CharField(choices=STREETS, max_length=4, default='', verbose_name='Вулиці')
+    track_number = models.CharField(max_length=13, verbose_name='Track number')
+    recipient_street = models.CharField(choices=STREETS, max_length=4, verbose_name='Recipient street')
+    delivered_date = models.DateTimeField()
 
     def __str__(self):
-        return self.streets
+        return f'{self.track_number}: {self.recipient_street}'
 
     class Meta:
-        verbose_name='вулиця'
-        verbose_name_plural='Вулиці'
+        ordering = ['delivered_date']
+        verbose_name = 'postage item'
+        verbose_name_plural = 'Postage items'
