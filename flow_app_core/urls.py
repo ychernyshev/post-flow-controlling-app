60652f58df9b7cb6ea2d13f17755b2035e0cd1bd
Eugene <eugene_chernyshev@yahoo.com>
2025-12-27 20:51:13 +0200

    Update urls.py: Search route
    Search route has been added

--- flow_app_core/urls.py
+++ flow_app_core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
+import flow_app_core
 from flow_app_core.views import DataPostageItemViewSet
 
 router = routers.DefaultRouter()
@@ -8,5 +9,6 @@ router = routers.DefaultRouter()
 router.register(r'postage', DataPostageItemViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('api/search/', flow_app_core.views.search_engine, name='search_items'),
 ]
