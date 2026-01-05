76a32a621595b1963625657128afbaaadcf1da9d
Eugene <eugene_chernyshev@yahoo.com>
2026-01-05 15:56:54 +0200

    Update StreetsPage.vue: page example
    Added page example

--- flow_app_ui/src/views/Dashboard.vue
+++ flow_app_ui/src/views/Dashboard.vue
@@ -16,7 +16,7 @@
 <!--      </button>-->
 <!--    </div>-->
 <!--  </div>-->
-  
+
   <main class="main-content mt-1 border-radius-lg">
     <!-- Navbar -->
     <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
